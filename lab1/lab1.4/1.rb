d8e616a5291a6e37817104ce2315c46bcff97eaf
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-03-03 22:59:45 +0300

    task4 method3 left_shift! and left_shift done

--- lab1/lab1.4/1.rb
+++ lab1/lab1.4/1.rb
@@ -2,7 +2,7 @@ def array_max?(arr, candidate_index)
     return arr.max() == arr[candidate_index]
 
 def local_min?(arr, candidate_index)
-    
+
     if candidate_index == 0 and arr[0] < arr[1]
         return true
     elsif candidate_index == arr.size()-1 and arr[candidate_index]< arr[candidate_index - 1]
@@ -13,4 +13,15 @@ def local_min?(arr, candidate_index)
         return false
     end
 
+end
+
+
+def left_shift!(arr)
+    return arr.append(arr.shift())
+end
+
+def left_shift(arr)
+    new_arr = Array.new arr
+   
+    return left_shift!(new_arr)
 end
