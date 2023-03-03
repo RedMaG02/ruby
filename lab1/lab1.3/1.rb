a64ca44ca3228c181a60bb0dcd2815436b0479af
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-03-03 20:56:52 +0300

    user input done

--- lab1/lab1.3/1.rb
+++ lab1/lab1.3/1.rb
@@ -21,3 +21,18 @@ def get_index_of_first_positive(arr)
 
     return nil
 end
+
+
+def main()
+    arr = (File.readlines ARGV[1]).map{ |str| str.to_i }
+
+    case ARGV[0].to_i
+    when 1
+        puts "Min value: #{get_min(arr)}"
+    when 2
+        puts "Index of first positive: #{get_index_of_first_positive(arr)}"
+    else
+        puts "Error"
+    end
+end
+
