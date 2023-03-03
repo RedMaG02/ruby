7e6aa4b4845cebe99498db7d9c96accd148f82d1
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-03-03 18:39:05 +0300

    lab1.2 method 2 done

--- lab1/lab1.2/1.rb
+++ lab1/lab1.2/1.rb
@@ -2,8 +2,27 @@ def prime?(x)
     return (2..x-1).count{|i| x % i == 0} == 0
 end
 
-def find_max_prime_divider(x)
+def get_max_prime_divider(x)
     x.downto(1).each { |div| if prime?(div) and x % div == 0 then return div end}
 end
 
-puts find_max_prime_divider 15
+
+def get_product_of_digits_that_not_divided_by5(x)
+    result = 1
+    count = 0
+
+    num.digits.each 
+    { 
+        |digit| 
+
+        if digit % 5 != 0
+            result *= digit
+            count++ 
+        end
+    }
+
+    if(count != 0)
+        return result
+    end
+
+end
