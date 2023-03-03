34d935dfc467f996ee47559dea5430041145be15
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-03-03 22:40:22 +0300

    task4 method2 done

--- lab1/lab1.4/1.rb
+++ lab1/lab1.4/1.rb
@@ -1,2 +1,16 @@
 def array_max?(arr, candidate_index)
     return arr.max() == arr[candidate_index]
+
+def local_min?(arr, candidate_index)
+    
+    if candidate_index == 0 and arr[0] < arr[1]
+        return true
+    elsif candidate_index == arr.size()-1 and arr[candidate_index]< arr[candidate_index - 1]
+        return true
+    elsif arr[candidate_index]< arr[candidate_index + 1] and arr[candidate_index]< arr[candidate_index - 1]
+        return true
+    else
+        return false
+    end
+
+end
