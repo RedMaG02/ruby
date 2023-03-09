eec821d1807f1ad04ba191b95f5af5a0aca7d2e4
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-03-09 22:00:03 +0300

    class student created

---
+++ lab2/student.rb
@@ -0,0 +1,9 @@
+class Student
+
+    attr_accessor :id :name :surname :patronymic :phone :telegram_username :email :github_username
+
+    def initialize(id:nil, name, surname, patronymic, phone:nil, telegram_username:nil, email:nil, github_username:nil)
+        self.
+    end
+
+end
