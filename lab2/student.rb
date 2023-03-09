7c4e4011113efb9cd0bda9fd48033386a2e07ce0
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-03-10 02:44:17 +0300

    validation for all fields in class Student

--- lab2/student.rb
+++ lab2/student.rb
@@ -7,9 +7,79 @@ class Student
         if valid_phone?(value)
             @phone = value
         else
-            raise StandartError.new "Phone in uncorrect format"
+            raise StandartError.new "phone in uncorrect format"
         end
-        
+
+    end
+
+    def id= value
+
+        if valid_id?(value)
+            @id = value
+        else
+            raise StandartError.new "id in uncorrect format"
+        end
+
+    end
+
+    def name= value
+
+        if valid_name?(value)
+            @name = value
+        else
+            raise StandartError.new "name in uncorrect format"
+        end
+
+    end
+
+    def surname= value
+
+        if valid_surname?(value)
+            @surname = value
+        else
+            raise StandartError.new "surname in uncorrect format"
+        end
+
+    end
+
+    def patronymic= value
+
+        if valid_patronymic?(value)
+            @patronymic = value
+        else
+            raise StandartError.new "patronymic in uncorrect format"
+        end
+
+    end
+
+    def telegram_username= value
+
+        if valid_telegram_username?(value)
+            @telegram_username = value
+        else
+            raise StandartError.new "telegram_username in uncorrect format"
+        end
+
+    end
+
+    def email= value
+
+        if valid_email?(value)
+            @email = value
+        else
+            raise StandartError.new "email in uncorrect format"
+        end
+
+    end
+
+    def github_username= value
+
+        if valid_github_username?(value)
+            @github_username = value
+        else
+            raise StandartError.new "github_username in uncorrect format"
+        end
+
     end
 
     def initialize(id:nil, name:, surname:, patronymic:, phone:nil, telegram_username:nil, email:nil, github_username:nil)
@@ -36,10 +106,59 @@ class Student
         return [id, surname, name, patronymic, phone, telegram_username, email, github_username].compact.join
     end
 
-    def valid_phone?(phone)
+    def valid_phone?(value)
 
         valid_reg = \d{11}
-        return phone =~ valid_reg
+        return value =~ valid_reg
+
+    end
+
+    def valid_name?(value)
+
+        valid_reg = /^[а-яА-Яa-zA-Z]+$/
+        return value =~ valid_reg
+
+    end
+
+    def valid_surname?(value)
+
+        valid_reg = /^[а-яА-Яa-zA-Z]+$/
+        return value =~ valid_reg
+
+    end
+
+    def valid_patronymic?(value)
+
+        valid_reg = /^[а-яА-Яa-zA-Z]+$/
+        return value =~ valid_reg
+
+    end
+
+    def valid_telegram_username?(value)
+
+        valid_reg = /^\@[a-zA-Z]([a-zA-Z]|\d|_){4,32}$/
+        return value =~ valid_reg
+
+    end
+
+    def valid_github_username?(value)
+
+        valid_reg = /^[a-zA-Z0-9]+$/
+        return value =~ valid_reg
+
+    end
+
+    def valid_email?(value)
+
+        valid_reg = /^[a-zA-Z0-9._]+\@[a-zA-Z0-9.]+\.[a-z]+$/
+        return value =~ valid_reg
+
+    end
+
+    def valid_id?(value)
+
+        valid_reg = \d+
+        return value =~ valid_reg
 
     end
 
