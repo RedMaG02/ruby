e99e925fa6e4e81a5101534fcd21323a97849589
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-03-24 22:24:54 +0300

    get_rows + get_cols

--- students_DB_ed_project/data_table.rb
+++ students_DB_ed_project/data_table.rb
@@ -10,4 +10,13 @@ class DataTable
   def get_element(row:, col:)
     return self.table[row][col]
   end
+
+  def get_rows_number
+    return self.table.size
+  end
+
+  def get_cols_number
+    return 0 if table.size == 0
+    return self.table[0].size
+  end
 end
